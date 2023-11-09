b4eb48ff409b6539adb5a3f20a179591216cf4a0
Davit_Muradyan <davit.muradyan@locator.am>
2023-11-09 18:12:04 +0400

    4 Leasson

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <script setup>
+import { ref, computed } from 'vue';
+
+const products = ref([
+    'Apple',
+    'Notebook',
+    'Phone',
+    'Camera',
+    'Samsung',
+    'Xiaomi',
+    'Shoes',
+    'T-Shirt',
+    'BMW'
+]);
+
+const query = ref('')
+const queryProducts = computed(() => {
+  return products.value.filter((product) => product.indexOf(query.value) !== -1);
+})
 </script>
 
 <template>
 <div>
+  <input type="search"
+         name="search"
+         placeholder="Product Search"
+         v-model="query"
+  >
+  <br><br><br>
+  {{ query }}
+  <br><br><br>
 
+  <ul>
+    <li v-for="product in queryProducts"
+        :key="product">
+      {{ product }}
+    </li>
+  </ul>
 </div>
 </template>
 
