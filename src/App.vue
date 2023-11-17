1b5be9f7e44696895bd2a44950e657dc2f3f9ecd
Davit_Muradyan <davit.muradyan@locator.am>
2023-11-17 18:52:19 +0400

    11.17

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,29 @@
 import { ref, computed } from 'vue';
 
 const products = ref([
-    'Apple',
-    'Notebook',
-    'Phone',
-    'Camera',
-    'Samsung',
-    'Xiaomi',
-    'Shoes',
-    'T-Shirt',
-    'BMW'
+  {title: 'Apple', price: 100.65},
+  {title: 'Notebook', price: 700.50},
+  {title: 'Phone', price: 1100.70},
+  {title: 'Camera', price: 10000.5},
+  {title: 'Samsung', price: 800.50},
+  {title: 'Xiaomi', price: 90.50},
+  {title: 'Shoes', price: 25.90},
+  {title: 'T-Shirt', price: 99.99},
+  {title: 'BMW', price: 59999.50}
 ]);
 
 const query = ref('')
 const queryProducts = computed(() => {
-  return products.value.filter((product) => product.indexOf(query.value) !== -1);
+  let product = products.value;
+  let search = query.value;
+
+  if (search) {
+    product = product.filter((p) => {
+      return p.title.indexOf(search) !== -1 ||
+      p.price.toString().indexOf(search) !== -1
+    })
+  }
+  return product;
 })
 </script>
 
@@ -33,7 +42,8 @@ const queryProducts = computed(() => {
   <ul>
     <li v-for="product in queryProducts"
         :key="product">
-      {{ product }}
+      {{ product.title }}
+      <sup>{{ product.price.toLocaleString() }}💲</sup>
     </li>
   </ul>
 </div>
